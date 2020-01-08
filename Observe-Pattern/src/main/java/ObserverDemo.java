7dc7239e4496d361d9aed995f4af666a06db5976
meta-chen <meta_chen@163.com>
2020-01-08 23:12:11 +0800

    观察者模式

---
+++ Observe-Pattern/src/main/java/ObserverDemo.java
@@ -0,0 +1,19 @@
+public class ObserverDemo {
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+
+        new OctalObserver(subject);
+        new BinaryObserver(subject);
+
+        System.out.println("First state change: 15");
+        subject.setState(15);
+        System.out.println("Second state change: 10");
+        subject.setState(10);
+    }
+}
+
+
+/*观察者模式
+* 何时使用：一个对象（目标对象）的状态发生改变，所有的依赖对象（观察者对象）都将得到通知，进行广播通知。
+*
+* */
